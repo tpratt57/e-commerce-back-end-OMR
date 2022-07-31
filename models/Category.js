071a397119295bd7d89cb0c56a221b5a2d7d5561
unknown <pratttrevor79@gmail.com>
2022-07-31 16:32:31 -0500

    updating root data & adding category requirements for model

--- models/Category.js
+++ models/Category.js
@@ -5,9 +5,20 @@ const sequelize = require('../config/connection.js');
 class Category extends Model {}
 
 Category.init(
-  /* {
+  {
     // define columns
-  }, */
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    categoryName:{
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, 
   {
     sequelize,
     timestamps: false,
